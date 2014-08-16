bb341f6eaeef8b2522ea85522679c70b386e77f7
Yuval Wyborski <finixy7@walla.com>
2014-08-16 15:54:04 +0300

    Implement Tetris class initialization

---
+++ tetris.py
@@ -0,0 +1,67 @@
+from piece import *
+from random import choice
+from utils import Vector
+
+import time
+
+DEFAULT_WIDTH = 10
+DEFAULT_HEIGHT = 22
+DEFAULT_SIZE = 4
+
+STARTING_SPEED = 5
+SPEED_INCREMENT = 0.1
+
+
+
+COLORS = [
+	(255, 0, 0),
+	(0, 255, 0),
+	(0, 0, 255),
+	(0, 255, 255)
+]
+
+class Tetris(object):
+	'''Manage the tetris game, call update to move the game a step forward.'''
+
+	def __init__(self, width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT, piece_size = DEFAULT_SIZE):
+		self.width = width
+		self.height = height
+		self.piece_size = piece_size
+		self.reset()
+
+	def new_piece(self, size):
+		piece = Piece.Random( size, choice(COLORS) )
+		piece.move( Vector((self.width // 2, 0)) )
+		return piece
+
+	def reset(self):
+		self.board = []
+		self.current_piece = None
+		self.lines_cleared = 0
+		self.speed = STARTING_SPEED
+
+	def update(self):
+		if self.current_piece is None:
+			self.current_piece = self.new_piece(self.piece_size)
+
+		self.current_piece.move(DOWN)
+
+	def pprint(self):
+		occupied = {}
+		for block in self.current_piece:
+			occupied[block.pos] = 'c'
+		for idx, block in enumerate(self.board):
+			occupied[block.pos] = idx
+
+		for y in range(self.height):
+			for x in range(self.width):
+				print(occupied.get((x, y), '.'),end = ' ')
+			print()
+
+if __name__ == '__main__':
+	t = Tetris()
+
+	for _ in range(11):
+		t.update()
+		t.pprint()
+		time.sleep(2)
